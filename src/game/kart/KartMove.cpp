0cdc429f15389e79c36e3db23d3fa5079ea07381
TheLordScruffy <thetruescruffy@gmail.com>
2022-11-30 21:31:15 -0500

    Convert flags to bitfields

--- src/game/kart/KartMove.cpp
+++ src/game/kart/KartMove.cpp
@@ -9,11 +9,11 @@ REPLACE(0x805799AC, void kart::KartMove::Respawn())
 {
     auto state = m_accessor->m_state;
 
-    if (state->m_flags4 & KartState::FLAGS4_ONLINE_REMOTE)
+    if (state->b_OnlineRemote)
         return;
 
-    if (state->m_flags4 & KartState::FLAGS4_POST_BATTLE_SIDELINE) {
-        state->m_flags0 |= KartState::FLAGS0_PRE_RESPAWN;
+    if (state->b_PostBattleSideline) {
+        state->b_PreRespawn = 1;
         return;
     }
 
@@ -24,11 +24,11 @@ REPLACE(0x80581824, void kart::KartMove::BattleFlagsUpdate())
 {
     auto state = m_accessor->m_state;
 
-    if (state->m_flags1 & KartState::FLAGS1_BLINKING) {
+    if (state->b_Blinking) {
         m_battleBlinkTimer--;
 
         if (m_battleBlinkTimer < 1) {
-            state->m_flags1 &= ~KartState::FLAGS1_BLINKING;
+            state->b_Blinking = 0;
             GetKartBlink()->Calc(1);
         } else {
             GetKartBlink()->Calc(0);
@@ -37,25 +37,18 @@ REPLACE(0x80581824, void kart::KartMove::BattleFlagsUpdate())
 
     if (sys::RaceConfig::s_instance->m_currentRace.m_legacyBattleMode ==
           sys::RaceConfig::RaceSetting::BATTLE_BALLOON &&
-        (state->m_flags4 & KartState::FLAGS4_BATTLE_SIDELINE) &&
-        !(state->m_flags4 & KartState::FLAGS4_ONLINE_REMOTE)) {
-        //!(state->m_flags0 & KartState::FLAGS0_PRE_RESPAWN) &&
-        //!(state->m_flags2 & KartState::FLAGS2_MAYBE_POST_RESPAWN) &&
-        //!(state->m_flags2 & KartState::FLAGS2_MAYBE_POST_RESPAWN2) &&
-        if (m_battleVanishTimer++ > 60) {
-            // Make the player shrink and disappear
-            if (!(state->m_flags2 & (KartState::FLAGS2_HAS_STOPPED |
-                                      KartState::FLAGS2_HAS_VANISHED))) {
-                state->m_flags2 |= KartState::FLAGS2_HAS_STOPPED;
-            }
-
-            // Remove the player's map icon and name tag
-            sys::RaceManager::s_instance->m_players[GetPlayerID()]->m_flags |=
-              0x10;
-
-            m_battleVanishTimer = 0;
-            state->m_flags4 &= ~KartState::FLAGS4_BATTLE_SIDELINE;
-            state->m_flags4 |= KartState::FLAGS4_POST_BATTLE_SIDELINE;
+        state->b_BattleSideline && !state->b_OnlineRemote &&
+        (m_battleVanishTimer++ > 60)) {
+        // Make the player shrink and disappear
+        if (!state->b_HasStoppedB2 && !state->b_HasVanished) {
+            state->b_HasStoppedB2 = 1;
         }
+
+        // Remove the player's map icon and name tag
+        sys::RaceManager::s_instance->m_players[GetPlayerID()]->m_flags |= 0x10;
+
+        m_battleVanishTimer = 0;
+        state->b_BattleSideline = 0;
+        state->b_PostBattleSideline = 1;
     }
 }
--- src/game/kart/KartState.h
+++ src/game/kart/KartState.h
@@ -8,6 +8,7 @@ class KartState
 public:
     FILL(0x0, 0x4);
 
+#if 0
     enum {
         FLAGS0_ACCELERATE = (1 << 0), // 0x1
         FLAGS0_BRAKE = (1 << 1), // 0x2
@@ -114,31 +115,161 @@ public:
     };
 
     enum {
-        FLAGS3_UNKNOWN_WALL_COLLISION = (1 << 3), // 0x8
-        FLAGS3_HWG = (1 << 5), // 0x20
-        FLAGS3_CHARGE_START_BOOST = (1 << 8), // 0x100
-    };
-
-    enum {
         FLAGS4_CPU = (1 << 0), // 0x1
         FLAGS4_LOCAL = (1 << 1), // 0x2
         FLAGS4_ONLINE_LOCAL = (1 << 2), // 0x4
         FLAGS4_ONLINE_REMOTE = (1 << 3), // 0x8
         FLAGS4_AUTOMATIC_DRIFT = (1 << 4), // 0x10
-        FLAGS4_GHOST = (1 << 5), // 0x40
+        FLAGS4_UNKNOWN_4_5 = (1 << 5), // 0x20
+        FLAGS4_GHOST = (1 << 6), // 0x40
+        FLAGS4_UNKNOWN_4_7 = (1 << 7), // 0x80
+        FLAGS4_UNKNOWN_4_8 = (1 << 8), // 0x100
+        FLAGS4_UNKNOWN_4_9 = (1 << 9), // 0x200
+        FLAGS4_UNKNOWN_4_10 = (1 << 10), // 0x400
+        FLAGS4_UNKNOWN_4_11 = (1 << 11), // 0x800
         FLAGS4_HAS_STOPPED = (1 << 12), // 0x1000
+        FLAGS4_UNKNOWN_4_13 = (1 << 13), // 0x2000
+        FLAGS4_UNKNOWN_4_14 = (1 << 14), // 0x4000
+        FLAGS4_UNKNOWN_4_15 = (1 << 15), // 0x8000
         FLAGS4_BATTLE_SIDELINE = (1 << 16), // 0x10000
-        FLAGS4_TRIGGER_RESPAWN = (1 << 17), // 0x20000
+        FLAGS4_BATTLE_RESPAWN = (1 << 17), // 0x20000
 
         // These flags are added:
         FLAGS4_POST_BATTLE_SIDELINE = (1 << 31), // 0x80000000
     };
+#endif
 
-    u32 m_flags0;
-    u32 m_flags1;
-    u32 m_flags2;
-    u32 m_flags3;
-    u32 m_flags4;
+    // Converted flags to bitfields
+
+    // Bitfield 0
+    u32 b_RampBoost : 1; // (1 << 31), 0x80000000
+    u32 b_JumpPad : 1; // (1 << 30), 0x40000000
+    u32 b_Wheelie : 1; // (1 < 29), 0x20000000
+    u32 b_DriftAuto : 1; // (1 << 28), 0x10000000
+    u32 b_SlipdriftCharge : 1; // (1 << 27), 0x8000000
+    u32 b_MushroomBoost : 1; // (1 << 26), 0x4000000
+    u32 b_UnknownExplosion : 1; // (1 << 25), 0x2000000
+    u32 b_StickRight : 1; // (1 << 24), 0x1000000
+    u32 b_AirStart : 1; // (1 << 23), 0x800000
+    u32 b_Unknown0_22 : 1; // (1 << 22), 0x400000
+    u32 b_Unknown0_21 : 1; // (1 << 21), 0x200000
+    u32 b_Boost : 1; // (1 << 20), 0x100000
+    u32 b_Hop : 1; // (1 << 19), 0x80000
+    u32 b_Ground : 1; // (1 << 18), 0x40000
+    u32 b_Unknown017 : 1; // (1 << 17), 0x20000
+    u32 b_StickyRoad : 1; // (1 << 16), 0x10000
+    u32 b_AirtimeOver20 : 1; // (1 << 15), 0x8000
+    u32 b_Unknown0_14 : 1; // (1 << 14), 0x4000
+    u32 b_StickLeft : 1; // (1 << 13), 0x2000
+    u32 b_AllWheelsCollision : 1; // (1 << 12), 0x1000
+    u32 b_AnyWheelCollision : 1; // (1 << 11), 0x800
+    u32 b_VehicleBodyFloorCollision : 1; // (1 << 10), 0x400
+    u32 b_GroundStart : 1; // (1 << 9), 0x200
+    u32 b_AccelerateStart : 1; // (1 << 8), 0x100
+    u32 b_HopStart : 1; // (1 << 7), 0x80
+    u32 b_WallCollision : 1; // (1 << 6), 0x40
+    u32 b_Wall3Collision : 1; // (1 << 5), 0x20
+    u32 b_PreRespawn : 1; // (1 << 4), 0x10
+    u32 b_DriftManual : 1; // (1 << 3), 0x8
+    u32 b_DriftInput : 1; // (1 << 2), 0x4
+    u32 b_Brake : 1; // (1 << 1), 0x2
+    u32 b_Accelerate : 1; // (1 << 0), 0x1
+
+    // Bitfield 1
+    u32 b_InAStar : 1; // (1 << 31), 0x80000000
+    u32 b_Trickable : 1; // (1 << 30), 0x40000000
+    u32 b_Unknown1_29 : 1; // (1 < 29), 0x20000000
+    u32 b_Unknown1_28 : 1; // (1 << 28), 0x10000000
+    u32 b_Unknown1_27 : 1; // (1 << 27), 0x8000000
+    u32 b_Unknown1_26 : 1; // (1 << 26), 0x4000000
+    u32 b_Unknown1_25 : 1; // (1 << 25), 0x2000000
+    u32 b_Unknown1_24 : 1; // (1 << 24), 0x1000000
+    u32 b_Unknown1_23 : 1; // (1 << 23), 0x800000
+    u32 b_HasTrickRot : 1; // (1 << 22), 0x400000
+    u32 b_Unknown121 : 1; // (1 << 21), 0x200000
+    u32 b_MiniturboBoost : 1; // (1 << 20), 0x100000
+    u32 b_Unknown1_19 : 1; // (1 << 19), 0x80000
+    u32 b_Unknown1_18 : 1; // (1 << 18), 0x40000
+    u32 b_Unknown1_17 : 1; // (1 << 17), 0x20000
+    u32 b_Unknown1_16 : 1; // (1 << 16), 0x10000
+    u32 b_ZipperTrick : 1; // (1 << 15), 0x8000
+    u32 b_Unknown1_14 : 1; // (1 << 14), 0x4000
+    u32 b_ZipperBoost : 1; // (1 << 13), 0x2000
+    u32 b_Unknown1_12 : 1; // (1 << 12), 0x1000
+    u32 b_MushroomCollision : 1; // (1 << 11), 0x800
+    u32 b_OverZipper : 1; // (1 << 10), 0x400
+    u32 b_ZipperCollision : 1; // (1 << 9), 0x200
+    u32 b_Blinking : 1; // (1 << 8), 0x100
+    u32 b_BoostOffroadInvincibility : 1; // (1 << 7), 0x80
+    u32 b_InATrick : 1; // (1 << 6), 0x40
+    u32 b_TrickStart : 1; // (1 << 5), 0x20
+    u32 b_InCannon : 1; // (1 << 4), 0x10
+    u32 b_CannonStart : 1; // (1 << 3), 0x8
+    u32 b_Unknown1_2 : 1; // (1 << 2), 0x4
+    u32 b_RespawnStart : 1; // (1 << 1), 0x2
+    u32 b_HitItemOrObj : 1; // (1 << 0), 0x1
+
+    // Bitfield 2
+    u32 b_Unknown2_31 : 1; // (1 << 31), 0x80000000
+    u32 b_Unknown2_30 : 1; // (1 << 30), 0x40000000
+    u32 b_HasThunderCloud : 1; // (1 < 29), 0x20000000
+    u32 b_InkApplied : 1; // (1 << 28), 0x10000000
+    u32 b_InABullet : 1; // (1 << 27), 0x8000000
+    u32 b_Unknown2_26 : 1; // (1 << 26), 0x4000000
+    u32 b_Unknown2_25 : 1; // (1 << 25), 0x2000000
+    u32 b_Unknown2_24 : 1; // (1 << 24), 0x1000000
+    u32 b_Unknown2_23 : 1; // (1 << 23), 0x800000
+    u32 b_HasJumpPadDir : 1; // (1 << 22), 0x400000
+    u32 b_Unknown2_21 : 1; // (1 << 21), 0x200000
+    u32 b_Unknown2_20 : 1; // (1 << 20), 0x100000
+    u32 b_HasVanished : 1; // (1 << 19), 0x80000
+    u32 b_HasStoppedB2 : 1; // (1 << 18), 0x40000
+    u32 b_Unknown2_17 : 1; // (1 << 17), 0x20000
+    u32 b_Crushed : 1; // (1 << 16), 0x10000
+    u32 b_InAMega : 1; // (1 << 15), 0x8000
+    u32 b_MaybePostRespawn2 : 1; // (1 << 14), 0x4000
+    u32 b_MaybePostRespawn : 1; // (1 << 13), 0x2000
+    u32 b_Unknown2_12 : 1; // (1 << 12), 0x1000
+    u32 b_Unknown2_11 : 1; // (1 << 11), 0x800
+    u32 b_Unknown2_10 : 1; // (1 << 10), 0x400
+    u32 b_Unknown2_9 : 1; // (1 << 9), 0x200
+    u32 b_Unknown2_8 : 1; // (1 << 8), 0x100
+    u32 b_Unknown2_7 : 1; // (1 << 7), 0x80
+    u32 b_Unknown2_6 : 1; // (1 << 6), 0x40
+    u32 b_Shocked : 1; // (1 << 5), 0x20
+    u32 b_WheelieRot : 1; // (1 << 4), 0x10
+    u32 b_Unknown2_3 : 1; // (1 << 3), 0x8
+    u32 b_SomethingWheelieBump : 1; // (1 << 2), 0x4
+    u32 b_InASlipstream : 1; // (1 << 1), 0x2
+    u32 b_ChargingASlipstream : 1; // (1 << 0), 0x1
+
+    // Unknown flags
+    u32 m_bitfield3;
+
+    // Bitfield 4
+    // These flags are added:
+    u32 b_PostBattleSideline : 1; // (1 << 31) 0x80000000
+    u32 b_Pad4 : 13;
+
+    // These flags are original:
+    u32 b_BattleRespawn : 1; // (1 << 17), 0x20000
+    u32 b_BattleSideline : 1; // (1 << 16), 0x10000
+    u32 b_Unknown4_15 : 1; // (1 << 15), 0x8000
+    u32 b_Unknown4_14 : 1; // (1 << 14), 0x4000
+    u32 b_Unknown4_13 : 1; // (1 << 13), 0x2000
+    u32 b_HasStoppedB4 : 1; // (1 << 12), 0x1000
+    u32 b_Unknown4_11 : 1; // (1 << 11), 0x800
+    u32 b_Unknown4_10 : 1; // (1 << 10), 0x400
+    u32 b_Unknown4_9 : 1; // (1 << 9), 0x200
+    u32 b_Unknown4_8 : 1; // (1 << 8), 0x100
+    u32 b_Unknown4_7 : 1; // (1 << 7), 0x80
+    u32 b_Ghost : 1; // (1 << 6), 0x40
+    u32 b_Unknown45 : 1; // (1 << 5), 0x20
+    u32 b_AutomaticDrift : 1; // (1 << 4), 0x10
+    u32 b_OnlineRemote : 1; // (1 << 3), 0x8
+    u32 b_OnlineLocal : 1; // (1 << 2), 0x4
+    u32 b_Local : 1; // (1 << 1), 0x2
+    u32 b_CPU : 1; // (1 << 0), 0x1
 };
 
 } // namespace kart
