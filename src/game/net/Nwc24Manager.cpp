dfae8907232465248eb4dbe663b10e3e07c6e74a
TheLordScruffy <thetruescruffy@gmail.com>
2022-12-02 12:46:05 -0500

    Update Nwc24Manager.cpp

--- src/game/net/Nwc24Manager.cpp
+++ src/game/net/Nwc24Manager.cpp
@@ -2,11 +2,6 @@
 
 // Stub out the entire class
 
-REPLACE(0x806690F8, s32 Nwc24Manager_806690F8())
-{
-    return -1;
-}
-
 REPLACE(0x80668FA4, s32 Nwc24Manager_80668FA4())
 {
     return 0;
@@ -20,6 +15,11 @@ REPLACE(0x80668FAC, s32 Nwc24Manager_80668FAC())
 REPLACE(0x80668FE4, bool Nwc24Manager_80668FE4())
 {
     return false;
+}
+
+REPLACE(0x806690F8, s32 Nwc24Manager_806690F8())
+{
+    return -1;
 }
 
 REPLACE(0x80669264, void Nwc24Manager_80669264())
@@ -63,4 +63,14 @@ REPLACE(0x8066EB5C, bool Nwc24Manager_8066EB5C())
 
 REPLACE(0x8066F0CC, void Nwc24Manager_8066F0CC())
 {
+}
+
+REPLACE(0x80672E14, s32 Nwc24Manager_80672E14())
+{
+    return -1;
+}
+
+REPLACE(0x8067686C, bool Nwc24Manager_8067686C())
+{
+    return false;
 }
